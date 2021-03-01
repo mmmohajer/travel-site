719fe2ef2b5bdc00945b6ad300b4be2e846d4bb1
Mohammad Mohajer <55400075+mmmohajer@users.noreply.github.com>
2021-03-01 00:22:42 -0500

    test

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,32 +1,25 @@
-const Axios = require('axios')
-
 exports.handler = (event, context, callback) => {
 	const secretContent = `
 	<h3>Welcome to the Secret Area</h3>
-	<p>Here we can tell you the sky color is <strong>blue</strong>.</p>
+	<p>Here we can tell you the sky color is <strong>blue</strong></p>
 	`
 	let body
 
 	if (event.body) {
-		body = JSON.parse(event.body)
-    handleSubmit()
+		body = JSON.parse(event.body) 
 	} else {
 		body = {} 
 	}
 
-  async handleSubmit() {
+	if (body.password == "javascript") {
+		callback(null, {
+			statusCode: 200,
+			body: "Welcome to the super secret area"
+		}) 
+	} else {
+		callback(null, {
+			statusCode: 401
+		})
+	}
 
-    let user = new FormData();
-
-    user.append("app_name", "contact_application_csc")
-    user.append('username', "mmmohajer70")
-    user.append('password', "Pass4ContactApplication")
-
-    try {
-      const response = await Axios.post("https://v1.stormapi.com/users/login", user)
-      console.log(response)
-    } catch(err) {
-      console.log(err)
-    }
-  }
 }
