fc0f9472e7a776ff3b3d348656d70d844f1529b9
Mohammad Mohajer <55400075+mmmohajer@users.noreply.github.com>
2021-02-28 23:03:44 -0500

    Using proxy

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,25 +1,22 @@
-exports.handler = (event, context, callback) => {
-	const secretContent = `
-	<h3>Welcome to the Secret Area</h3>
-	<p>Here we can tell you the sky color is <strong>blue</strong>.</p>
-	`
-	let body
+const express = require('express')
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-	if (event.body) {
-		body = JSON.parse(event.body) 
-	} else {
-		body = {} 
-	}
+const app = express()
+const port = process.env.PORT || 3000
 
-	if (body.password == "javascript") {
-		callback(null, {
-			statusCode: 200,
-			body: secretContent
-		}) 
-	} else {
-		callback(null, {
-			statusCode: 401
-		})
-	}
+app.use('/users', createProxyMiddleware({ 
+  target: 'https://v1.stormapi.com/', 
+  changeOrigin: true 
+  })
+)
 
-}
+app.use('/.netlify', createProxyMiddleware({ 
+  target: 'https://condescending-payne-1beaf7.netlify.app', 
+  changeOrigin: true 
+  })
+)
+
+app.use(express.static(path.join(__dirname, '../app')));
+
+app.listen(port, () => console.log(`App is live on port ${port}!`))
